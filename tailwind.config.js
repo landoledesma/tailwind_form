7d444dcd49d6db19d7640855643dd7b4f93f3d84
lando <lando_ledesma@outlook.com>
2024-10-14 15:06:03 -0600

    desarrollando el formulario de regitro

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "laptop-image": "url('../assets/background-form.jpg')"
+      }, 
+      fontFamily: {
+        "poppins": "'Poppins', sans-serif",
+      }
+    },
   },
   plugins: [],
 }
-
